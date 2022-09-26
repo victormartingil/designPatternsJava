e8cc6b8240370f12867441df27ca20c56c804170
victor.martingil <victor.martingil@plexus.es>
2022-09-26 12:48:43 +0200

    Singleton

---
+++ src/main/java/org/martin/designpatters/creational/singleton/Connection.java
@@ -0,0 +1,23 @@
+package org.martin.designpatters.creational.singleton;
+
+/**
+ * @author victor.martingil
+ **/
+public class Connection {
+
+    private static Connection instance;
+
+    // We create a private constructor to avoid
+    // We cannot create new object with this constructor. Check next method.
+    private Connection(){}
+
+    //To create a new instance, due the constructor is private, we build the following method
+    // With this, we avoid to instantiate more than once one object from this Class.
+    // Must be static, because we need call this method before instantiate any object.
+    public static Connection getInstance(){
+        if (instance == null) {
+            instance = new Connection();
+        }
+        return instance;
+    }
+}
---
+++ src/main/java/org/martin/designpatters/creational/singleton/SingletonMain.java
@@ -0,0 +1,14 @@
+package org.martin.designpatters.creational.singleton;
+
+/**
+ * @author victor.martingil
+ **/
+public class SingletonMain {
+
+    public static void main (String[] args) {
+
+        Connection connection = Connection.getInstance();
+
+    }
+
+}
